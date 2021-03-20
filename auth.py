82473986159ad9880fef34281cdaa855ebaddef5
mjuc <69016700+mjuc@users.noreply.github.com>
2021-03-20 11:28:57 +0100

    Added user registration methods.
    Added simple templates for login and register pages.

--- auth.py
+++ auth.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
+
+
 
 
 @auth.route('/login')
@@ -9,7 +13,24 @@ def login():
     #login page code here
     pass
 
-@auth.route('/register')
+@auth.route('/register', methods=['POST'])
 def register():
-    #register page code here
-    pass
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        return redirect(url_for('auth.register'))
+
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    if user:
+        flash('Email address already exists')
+        return redirect(url_for('auth.register'))
+
+    return redirect(url_for('auth.login'))
